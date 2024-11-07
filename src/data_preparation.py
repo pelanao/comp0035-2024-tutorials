4ebeab1baad307f47de341f31c504e601da8512a
pelanao <danielggg16@gmail.com>
2024-11-07 18:48:17 +0000

    rearranged structure of functions for more logical flow

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -19,67 +19,6 @@ def describe_dataframe(DataFrame):
     print(DataFrame.describe(), "\n")
     return
 
-def change_datatype(df):
-    """ Change datatype of columns to desired format.
-        Columns in float64 are changed to int (requires debugging),
-        and columns related to dates are changed to datetime64
- 
-        Parameters:
-            DataFrame (DataFrame): an imported csv or excel in dataframe format, to be modified
- 
-        Returns:
-            modified dataframe
-    """
-
-    # change all float64 datatypes to int
-    columns_to_change= ['countries', 'events', 'participants_m', 'participants_f', 'participants']
-    for column in df:
-        # method1: change all columns in float64 datatype to int
-        if df[column].dtypes == 'float64':
-            # print(f"change {column}")
-            df[column] = df[column].astype(int)
-
-    # change date columns into appropriate DataFrame date format
-    df['start'] = pd.to_datetime(df['start'], format='%d/%m/%Y')
-    df['end'] = pd.to_datetime(df['end'], format='%d/%m/%Y')
-    # print(df.loc[:,['start','end']] )
-    return[df]
-
-def merge_npc(raw, npc):
-    """ Merge event data with the npc codes and prepare for further processing
-
-        Parameters:
-            raw (DataFrame): pandas dataframe with event data
-            npc (DataFrame): pandas dataframe with country codes
- 
-        Returns:
-            df_prepared(DataFrame): merged and prepared dataframe
-    """
-    # fix incompatible country names in the raw file
-    raw = country_name(raw)
-
-    # merge events_raw with npc_codes 
-    merged = raw.merge(npc, how='left', left_on='country', right_on='Name')
-    # print(f"\nMerged database country/codes: \n{merged.loc[:,['country', 'Code', 'Name']]}\n")
-
-    # delete unwanted columns
-    df_prepared = merged.drop(columns=['URL', 'disabilities_included', 'highlights', 'Name'], axis=1)
-
-    # print number of missing values in merged dataframe df_prepared
-    # print(f"\nMissing values in whole dataframe: \n{df_prepared.isna()}")
-
-    # delete unwanted rows
-    df_prepared = df_prepared.drop(index=[0,17,31])     #  drop rows for Rome 1960 and events set in the future due to missing data
-    df_prepared = df_prepared.reset_index(drop=True)   # reset index of DF to skip dropped rows
-    missing_rows = df_prepared[df_prepared.isna().any(axis=1)]  
-    # print(f"\nMissing rows:\n{missing_rows}\n")     # confirm no rows have missing data
-
-    # from 'type' column, remove whitespace and change to lowercase
-    df_prepared['type'] = df_prepared['type'].str.strip()
-    df_prepared['type'] = df_prepared['type'].str.lower()
-    return (df_prepared)
-
-
 def country_name(DF):
     """ Replace country abbreviations with compatible names
 
@@ -98,6 +37,72 @@ def country_name(DF):
     }
     fixed_DF = DF.replace(to_replace=replacement_names)     # replace abbrreviations with values found in the dictionary
     return(fixed_DF)
+
+def change_datatype(df):
+    """ Change datatype of columns to desired format.
+        Columns in float64 are changed to int, columns related to dates are changed to datetime64,
+        and columns are standardised to avoid categorisation issues
+ 
+        Parameters:
+            DataFrame (DataFrame): an imported csv or excel in dataframe format, to be modified
+ 
+        Returns:
+            modified dataframe
+    """
+
+    # change all float64 datatypes to int
+    for column in df:
+        # method1: change all columns in float64 datatype to int
+        if df[column].dtypes == 'float64':
+            # print(f"change {column}")
+            df[column] = df[column].astype(int)
+
+    # change date columns into appropriate DataFrame date format
+    df['start'] = pd.to_datetime(df['start'], format='%d/%m/%Y')
+    df['end'] = pd.to_datetime(df['end'], format='%d/%m/%Y')
+
+    
+
+
+    # standardise 'type' column by removing whitespace and changing to lowercase
+    df['type'] = df['type'].str.strip()
+    df['type'] = df['type'].str.lower()
+    return df
+
+def prepare_data(raw, npc):
+    """ Merge event data with the npc codes and prepare for further processing
+
+        Parameters:
+            raw (DataFrame): pandas dataframe with event data
+            npc (DataFrame): pandas dataframe with country codes
+ 
+        Returns:
+            df_prepared(DataFrame): merged and prepared dataframe
+    """
+    # fix incompatible country names in the events_raw file
+    raw = country_name(raw)
+
+    # merge events_raw with npc_codes
+    merged = raw.merge(npc, how='left', left_on='country', right_on='Name')
+
+    # delete unwanted columns
+    df_prepared = merged.drop(columns=['URL', 'disabilities_included', 'highlights', 'Name'], axis=1)
+
+    # delete unwanted rows
+    df_prepared = df_prepared.drop(index=[0,17,31])     #  drop rows for Rome 1960 and events set in the future due to missing data
+    df_prepared = df_prepared.reset_index(drop=True)    # reset index of DF to skip dropped rows
+
+    # standardise datatypes for further processing 
+    df_prepared = change_datatype(df_prepared)
+
+    # insert extra column with duration data
+    df_prepared.insert(df_prepared.columns.get_loc('end')+1, 'duration', (df_prepared['end'] - df_prepared['start']).dt.days.astype(int))
+    
+    # export dataframe to csv format
+    # pathlib.Path(__file__).parent
+    # df_prepared.to_csv()
+    return (df_prepared)
+
 
 def main():
     """"
@@ -132,8 +137,9 @@ def main():
         print(f"CSV file not found. Please check the file path. Error: {e}")
         exit()
     
+
     # call function to merge with npc codes
-    df_full = merge_npc(paralympics_events, npc_codes)
+    # df_full = merge_npc(paralympics_events, npc_codes)
 
     # # call the function to describe the dataframe
     # describe_dataframe(paralympics_events)
@@ -141,19 +147,12 @@ def main():
     # describe_dataframe(medal_standings)
     # describe_dataframe(npc_codes)
 
-    # change datatypes into desired format
-    change_datatype(df_full)
-    # exit()
-    # change_datatype(paralympics_all)
-
-    # print unique values
-    # print(df_full['type'].unique())           # returns the name of every unique name in that catergory
-    # print(df_full['type'].value_counts())     # counts the occurences of each
-
+    #  merge and prepare events_raw and npc_codes
+    prepared_df = prepare_data(paralympics_events, npc_codes)
     
-    df_full.insert(df_full.columns.get_loc('end')+1, 'duration', (df_full['end'] - df_full['start']).dt.days.astype(int))
-    # print(df_full)
-
+    # print unique values
+    # print(prepared_df['type'].unique())           # returns the name of every unique name in that catergory
+    # print(prepared_df['type'].value_counts())     # counts the occurences of each
 
 if __name__ == "__main__":
     main()
