492f411a6819bfe153749473e46da5a4933591b0
pelanao <danielggg16@gmail.com>
2024-11-07 17:10:20 +0000

    fixed missing values in merged database

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -50,17 +50,63 @@ def change_datatype(df):
     df['start'] = pd.to_datetime(df['start'], format='%d/%m/%Y')
     df['end'] = pd.to_datetime(df['end'], format='%d/%m/%Y')
     # print(df.loc[:,['start','end']] )
-
     return[df]
 
+def merge_npc(raw, npc):
+    """ Merge event data with the npc codes and prepare for further processing
 
+        Parameters:
+            raw (DataFrame): pandas dataframe with event data
+            npc (DataFrame): pandas dataframe with country codes
+ 
+        Returns:
+            merged(DataFrame): merged dataframe
+    """
+    # fix incompatible country names in the raw file
+    raw = country_name(raw)
+
+    # merge events_raw with npc_codes 
+    merged = raw.merge(npc, how='left', left_on='country', right_on='Name')
+    # print(f"\nMerged database country/codes: \n{merged.loc[:,['country', 'Code', 'Name']]}\n")
+
+    # delete unwanted columns
+    df_prepared = merged.drop(columns=['URL', 'disabilities_included', 'highlights', 'Name'], axis=1)
+
+    # print number of missing values in merged dataframe df_prepared
+    # print(f"\nMissing values in whole dataframe: \n{df_prepared.isna()}")
+
+    # delete unwanted rows
+    df_prepared = df_prepared.drop(index=[0,17,31])     #  drop rows for Rome 1960 and events set in the future due to missing data
+    df_prepared = df_prepared.reset_index(drop=True)   # reset index of DF to skip dropped rows
+    missing_rows = df_prepared[df_prepared.isna().any(axis=1)]  
+    # print(f"\nMissing rows:\n{missing_rows}\n")     # confirm no rows have missing data
+
+
+def country_name(DF):
+    """ Replace country abbreviations with compatible names
+
+        Parameters:
+            DF (DataFrame): an imported csv or excel in dataframe format, to be modified
+ 
+        Returns:
+            fixed_DF (DataFrame): modified dataframe with compatible country names
+    """
+    replacement_names = {                               # dictionary of abbreviations and respective country names
+        'UK': 'Great Britain',
+        'USA': 'United States of America',
+        'Korea': 'Republic of Korea',
+        'Russia': 'Russian Federation',
+        'China': "People's Republic of China"
+    }
+    fixed_DF = DF.replace(to_replace=replacement_names)     # replace abbrreviations with values found in the dictionary
+    return(fixed_DF)
 
 def main():
     """"
     Main logic for the program
     Reads the provided databases into DataFrame format
     """
-    # set display ooptions for 
+    # set display ooptions for pandas
     pd.set_option("display.max_columns", None)
 
     # read events_raw.csv file into DF
@@ -79,16 +125,18 @@ def main():
     except FileNotFoundError as e:
         print(f"Excel file not found. Please check the file path. Error: {e}")
         exit()
-
+    
     # read npc_codes.csv file into DF
     try:
         npc_codes_csv = pathlib.Path(__file__).parent/'tutorialpkg'/'data'/'npc_codes.csv'      # store provided .csv file
-        print(npc_codes_csv)
         npc_codes = pd.read_csv(npc_codes_csv, encoding='utf-8', encoding_errors='ignore', usecols=['Code', 'Name'])    # read csv file into DataFrame, accounting for encoding errors in csv file
     except FileNotFoundError as e:
         print(f"CSV file not found. Please check the file path. Error: {e}")
         exit()
     
+    # call function to merge with npc codes
+    merge_npc(paralympics_events, npc_codes)
+
     # # call the function to describe the dataframe
     # describe_dataframe(paralympics_events)
     # describe_dataframe(paralympics_all)
@@ -99,13 +147,6 @@ def main():
     # change_datatype(paralympics_events)
     # change_datatype(paralympics_all)
 
-    # merge events_raw with npc_codes 
-    full_para_events = paralympics_events.merge(npc_codes, how='left', left_on='country', right_on='Name')
-    print(full_para_events.loc[:,['country', 'Code', 'Name']])
-
-    # delete unwanted columns
-    df_prepared = full_para_events.drop(columns=['URL', 'disabilities_included', 'highlights'], axis=1)
-    print(df_prepared.columns)
 
 if __name__ == "__main__":
     main()
