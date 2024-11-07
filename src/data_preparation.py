89201417ade21d80fa5f98f8a03900c0fc873c97
pelanao <danielggg16@gmail.com>
2024-11-07 00:37:39 +0000

    added error handling and fixed main() function structure

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,5 +1,3 @@
-
-
 import pathlib
 import pandas as pd
 
@@ -14,13 +12,24 @@ def describe_dataframe(DataFrame):
 
 
 
-if __name__ == '__main__':
-    describe_dataframe("file")
-    paralympics_events_csv = pathlib.Path(__file__).parent / 'tutorialpkg' / 'data' / 'paralympics_events_raw.csv'      # store provided .csv file name
-    paralympics_events = pd.read_csv(paralympics_events_csv)                                                            # read csv file into DataFrame
+def main():
+    """"
+    Main logic for the program
+    Reads the provided databases into DataFrame format
+    """
+    
+    try:
+        paralympics_events_csv = pathlib.Path(__file__).parent/'tutorialpkg'/'data'/'paralympics_events_raw.csv'      # store provided .csv file
+        paralympics_events = pd.read_csv(paralympics_events_csv)                                                      # read csv file into DataFrame
+    except FileNotFoundError as e:
+        print(f"CSV file not found. Please check the file path. Error: {e}")
 
-    paralympics_all_xlsx = pathlib.Path(__file__).parent / 'tutorialpkg' /'data' / 'paralympics_all_raw.xlsx'            # store provided .xlsx file name
-    paralympics_all = pd.read_excel(paralympics_all_xlsx, sheet_name=0)                                                  # read first sheet of Excel file into DataFrame
-    medal_standings = pd.read_excel(paralympics_all_xlsx, sheet_name=1)                                                  # read second sheet of Excel file into DataFrame
+    try:
+        paralympics_all_xlsx = pathlib.Path(__file__).parent / 'tutorialpkg' /'data' / 'paralympics_all_raw.xlsx'            # store provided .xlsx file name
+        paralympics_all = pd.read_excel(paralympics_all_xlsx, sheet_name=0)                                                  # read first sheet of Excel file into DataFrame
+        medal_standings = pd.read_excel(paralympics_all_xlsx, sheet_name=1)                                                  # read second sheet of Excel file into DataFrame
+    except FileNotFoundError as e:
+        print(f"Excel file not found. Please check the file path. Error: {e}")
 
-    print()
+if __name__ == "__main__":
+    main()
